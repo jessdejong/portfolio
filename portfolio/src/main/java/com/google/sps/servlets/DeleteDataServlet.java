9786516a8419b44abc266902a9e47894a0fe2915
Jess DeJong <jessdejong843@gmail.com>
2020-06-05 13:38:10 +0000

    Delete comments inline

--- portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
@@ -17,12 +17,9 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -40,10 +37,8 @@ public class DeleteDataServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Remove all entities from datastore
     PreparedQuery results = datastore.prepare(commentsQuery);
-    List<Key> keys = new ArrayList<>();
     for (Entity entity : results.asIterable()) {
-      keys.add(entity.getKey());
+      datastore.delete(entity.getKey());
     }
-    datastore.delete(keys);
   }
 }
